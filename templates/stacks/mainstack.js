10f5138207897afc7836c7a319c6e5894e5bef0a
cw <chiewwei@greywolf.sg>
2024-04-04 18:00:24 +0800

    #Update fix properties for navbar horizontal

--- templates/stacks/mainstack.js
+++ templates/stacks/mainstack.js
@@ -8,53 +8,60 @@ const LiveTabNavigation = () => {
     const tabBarLabelStyle= {
         fontSize: 16,
     }
+    // TODO - all screen loaded on mount, should be loaded individually
     return (
-        <Tab.Navigator
-            initialRouteName="热门"
-            screenOptions={{
+        <>
+            <Tab.Navigator
+            initialRouteName="highlight"
+            swipeEnabled={true}
+            screenOptions={ ({route})=>({
                 tabBarActiveTintColor: globalParameters.color,
                 tabBarInactiveTintColor: '#CCCCCC',
-                tabBarStyle:{
-                    backgroundColor:'white',
-                },
+                tabBarScrollEnabled:true,
                 tabBarLabelStyle:{
                     fontSize:16,
                 },
-            }}
-        >
-            <Tab.Screen name="follow" component={FollowView}
+                tabBarItemStyle:{
+                    width:"auto"
+                }
+            })}
+            >
+                <Tab.Screen name="follow" component={FollowView}
                 options={{
-                    tabBarLabel:({color,focused})=>(<Text style={focused ? {fontWeight:800,fontSize:16,color} :{fontSize:16,color}}>关注</Text>)
+                    tabBarGap:0,
+                    tabBarLabel:({color,focused})=>(
+                        <Text style={focused ? {fontWeight:800,fontSize:16,color} :{fontSize:16,color}}>关注</Text>
+                    )
                 }} 
-            />
-            <Tab.Screen name="highlight" component={GroupMatchList} 
+                />
+                <Tab.Screen name="highlight" component={GroupMatchList} 
                 options={{
-                    tabBarLabel:({color,focused})=>(<Text style={focused ? {fontWeight:800,fontSize:16,color} :{fontSize:16,color}}>热门</Text>)
+                    tabBarLabel:({color,focused})=>(
+                        <Text style={focused ? {fontWeight:800,fontSize:16,color} :{fontSize:16,color}}>热门</Text>
+                    )
                 }}
-            />
-            <Tab.Screen name="足球" component={GroupMatchList} />
-            <Tab.Screen name="篮球" component={GroupMatchList} />
-            <Tab.Screen name="NBA" component={GroupMatchList} />
-            <Tab.Screen name="英超" component={GroupMatchList} />
-            {/* <Tab.Screen name="西甲" component={SettingsScreen} />
-            <Tab.Screen name="意甲" component={SettingsScreen} />
-            <Tab.Screen name="法甲" component={SettingsScreen} />
-            <Tab.Screen name="德甲" component={SettingsScreen} />
-            <Tab.Screen name="CBA" component={SettingsScreen} />
-            <Tab.Screen name="西杯" component={SettingsScreen} />
-            <Tab.Screen name="世界杯" component={SettingsScreen} />
-            <Tab.Screen name="英联杯" component={SettingsScreen} />
-            <Tab.Screen name="亚冠" component={SettingsScreen} />
-            <Tab.Screen name="英冠" component={SettingsScreen} />
-            <Tab.Screen name="荷甲" component={SettingsScreen} />
-            <Tab.Screen name="苏超" component={SettingsScreen} />
-            <Tab.Screen name="葡超" component={SettingsScreen} />
-            <Tab.Screen name="俄超" component={SettingsScreen} />
-            <Tab.Screen name="土超" component={SettingsScreen} />
-            <Tab.Screen name="荷甲" component={SettingsScreen} />
-            <Tab.Screen name="荷甲" component={SettingsScreen} />
-            <Tab.Screen name="荷甲" component={SettingsScreen} /> */}
-        </Tab.Navigator>
+                />
+                <Tab.Screen name="足球" component={FollowView} />
+                <Tab.Screen name="篮球" component={FollowView} />
+                <Tab.Screen name="NBA" component={FollowView} />
+                <Tab.Screen name="英超" component={FollowView} />
+                <Tab.Screen name="西甲" component={FollowView} />
+                <Tab.Screen name="意甲" component={FollowView} />
+                <Tab.Screen name="法甲" component={FollowView} />
+                <Tab.Screen name="德甲" component={FollowView} />
+                {/* <Tab.Screen name="CBA" component={FollowView} />
+                <Tab.Screen name="西杯" component={FollowView} />
+                <Tab.Screen name="世界杯" component={FollowView} />
+                <Tab.Screen name="英联杯" component={FollowView} />
+                <Tab.Screen name="亚冠" component={FollowView} />
+                <Tab.Screen name="英冠" component={FollowView} />
+                <Tab.Screen name="荷甲" component={FollowView} />
+                <Tab.Screen name="苏超" component={FollowView} />
+                <Tab.Screen name="葡超" component={FollowView} />
+                <Tab.Screen name="俄超" component={FollowView} />
+                <Tab.Screen name="土超" component={FollowView} /> */}
+            </Tab.Navigator>
+        </>
     )
 }
 export default LiveTabNavigation
